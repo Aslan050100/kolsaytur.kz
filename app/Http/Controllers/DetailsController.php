69827ab423b64d4e6848bb4f0f07172443517458
aslan050100 <aslan.00_00@mail.ru>
2020-01-27 21:44:36 +0600

    added template

---
+++ app/Http/Controllers/DetailsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DetailsController extends Controller
+{
+    //
+    public function index(){
+    	return view('details');
+    }
+}
