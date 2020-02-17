9b83a752b55bdd6915e576134aed31c5b1c9efec
aslan050100 <aslan.00_00@mail.ru>
2020-02-17 22:00:58 +0600

    Cors

--- public/index.php
+++ public/index.php
@@ -59,8 +59,5 @@ $response->send();
 
 $kernel->terminate($request, $response);
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS, post, get');
-header("Access-Control-Max-Age", "3600");
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
-header("Access-Control-Allow-Credentials", "true");
+header('Access-Control-Allow-Origin: *');  
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
