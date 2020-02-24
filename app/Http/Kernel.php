a8d790cc663ab34e972a34b2693954336b214ce4
aslan050100 <aslan.00_00@mail.ru>
2020-02-24 19:57:05 +0600

    Cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,8 +19,8 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        
-        \Fruitcake\Cors\HandleCors::class,
+      
+        \App\Http\Middleware\Cors::class,
     ];
 
     /**
@@ -42,7 +42,6 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \Fruitcake\Cors\HandleCors::class,
         ],
     ];
 
@@ -64,7 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'cors' => \Fruitcake\Cors\HandleCors::class,
+        'cors' => \App\Http\Middleware\Cors::class,
     ];
 
     /**
