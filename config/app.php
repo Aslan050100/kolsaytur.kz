3e62c5fd830fcddab0632a8fa9294db9e43bf0e5
aslan050100 <aslan.00_00@mail.ru>
2020-02-24 18:48:01 +0600

    Cors

--- config/app.php
+++ config/app.php
@@ -176,6 +176,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Intervention\Image\ImageServiceProvider::class,
+        Spatie\Cors\CorsServiceProvider::class,
     ],
 
     /*
