4f586b425c8c2323fbc1ad32b483dcec161350b3
aslan050100 <aslan.00_00@mail.ru>
2020-02-25 00:15:43 +0600

    Api

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 //Api
 Route::get('/getProducts','MainController@getProducts')->middleware('cors');
 Route::match(['get','post'],'/searchProducts','MainController@searchProducts')->middleware('cors');
-Route::get('/getProductByid/{id}','DetailsController@getProductByid')->middleware('cors');
+Route::match(['get','post'],'/getProductByid/{id}','DetailsController@getProductByid')->middleware('cors');
 Route::match(['get','post'],'/addOrder1/{id}','DetailsController@addOrder1')->middleware('cors');
 Route::get('/getRoom_types','DetailsController@getRoom_types')->middleware('cors');
 Route::match(['get','post'],'/checkIp/{id}','DetailsController@checkIp')->middleware('cors');
