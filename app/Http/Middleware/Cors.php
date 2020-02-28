b60e84c66c314c38c9dd8f8605974fdbb5f2eb4e
aslan050100 <aslan.00_00@mail.ru>
2020-02-28 20:03:10 +0600

    cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,8 +18,8 @@ class Cors
         header("Access-Control-Allow-Origin: *");
         //ALLOW OPTIONS METHOD
         $headers = [
-            'Access-Control-Allow-Methods' => 'POST,GET,OPTIONS,PUT,DELETE',
-            'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization',
+            'Access-Control-Allow-Methods' => '*',
+            'Access-Control-Allow-Headers' => '*',
         ];
         if ($request->getMethod() == "OPTIONS"){
             //The client-side application can set only headers allowed in Access-Control-Allow-Headers
